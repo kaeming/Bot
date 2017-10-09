929be6f922d435519e145a680b56734bb9fb2a83
dinisbcoelho <dinisbcoelho@sapo.pt>
2017-10-09 18:43:58 +0100

    Update blog-view.py

--- blog-view.py
+++ blog-view.py
@@ -3,20 +3,9 @@ import time
 from torrequest import TorRequest
 from user_agent import generate_user_agent
 
-print """\033[1m\033[37m
-     _____ ___  ____            ____  _              __     ___                     ____        _                                                     
-    |  ___/ _ \/ ___|          | __ )| | ___   __ _  \ \   / (_) _____      _____  | __ )  ___ | |_                                                   
-    | |_ | | | \___ \   _____  |  _ \| |/ _ \ / _` |  \ \ / /| |/ _ \ \ /\ / / __| |  _ \ / _ \| __|                                                  
-    |  _|| |_| |___) | |_____| | |_) | | (_) | (_| |   \ V / | |  __/\ V  V /\__ \ | |_) | (_) | |_                                                   
-    |_|   \___/|____/          |____/|_|\___/ \__, |    \_/  |_|\___| \_/\_/ |___/ |____/ \___/ \__|                                                  
-                                              |___/                                                                                                  
-
-                                                                     \033[41m FOS- Fools of Security :)
-\033[0m
-"""
-site = raw_input("Enter your Blog Address : ")
-blog = input("Enter The number of Viewers : ")
-
+print "Welcome to EVIL BOT"
+site = raw_input("Enter the Site Address : ")
+views = input("Enter The number of Viewers : ")
 
 def run():
 	ua = generate_user_agent()
@@ -29,10 +18,8 @@ headers = {
 #     print(response.text)  
      print "["+str(i)+"]" + " Blog View Added With IP:" +  tr.get('http://icanhazip.com').content
      tr.reset_identity()
-  
-
 
 if __name__ == '__main__':
         with TorRequest() as tr:
-	    for i in range(blog):
+	    for i in range(views):
 	          run()
